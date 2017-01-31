a6bdac88d73d1d80b9d171adafce68fa601f4518
Kateryna Huza <catherinaguza@gmail.com>
2017-01-31 22:56:31 +0200

    Add bootstrap, change ui

--- index.php
+++ index.php
@@ -1,27 +1,95 @@
-<?php
-$servername = "localhost";
-$username = "scada";
-$password = "123456";
-$dbname = "scada";
+<!doctype html>
+<html lang="ru">
+    <head>
+        <meta charset="UTF-8">
+        <title>Web SCADA</title>
+       <link href="css/style.css" rel="stylesheet" type="text/css" > 
+          <link href="css/bootstrap.min.css" rel="stylesheet">
+        <script type="text/javascript" src="js/jquery-1.11.1.min.js"></script>
+        <script type="text/javascript" src="js/canvasjs.min.js"></script>
+         <script src="js/bootstrap.min.js"></script>
+        
+    </head>
+    <body>
+        
+        
+        
+        <div id="wrapper">
+            <div id="inner-wrapper">
+                <header id="header">
+                    <img src="images/header.png" alt="header" />
+                </header>
 
-// Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
-// Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} else {
-    echo "SQL connection succeeded!";
-}
-
-$sql = "INSERT INTO `monitoring_info` (time, temp_1, temp_2, pressure_1)
-VALUES ('".date("Y-m-d H:i:s")."', 1".rand(2, 6).".".rand(0, 99).", 56.8, 59);";
-echo "Executing SQL-command: <br/><blockquote>".$sql."</blockquote><br/>";
 
 
-if ($conn->multi_query($sql) === true) {
-    echo "New records created successfully<br/>";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
-}
+                <div id="content">
+                                
+                    <div id="scrubber">
+                    <img src="images/scrubber1.png" alt="scrubber" />
+                    </div>
 
-$conn->close();
+                    <div id="right-part">
+                        <img src="images/operator.png" alt="operator" />
+
+                            <div id="form">
+                            <form>
+                                <p> Температура у скрубері:
+                                    <output name="Значення"><b id="temp"></b> <b><sup>o</sup>C</b></output> 
+                                </p>
+                                <p> Витрата NaOH:
+                                 <output name="temp"><b id="naohinp"></b> <b>кг/секундку</b></output>
+                                </p>
+                                <p> Концентрація CO<sub>2</sub> на вході:
+                                    <output name="Значення"><b id="co2inp"></b> <b>%</b></output>
+                                </p>
+                                <p> Витрата суміші C<sub>2</sub>H<sub>2</sub> та CO<sub>2</sub>:
+                                 <output name="Значення"><b id="massinp"></b> <b>кг/секундку</b></output>
+                                </p>
+                                <p> Витрата шламу:
+                                    <output name="Значення"><b id="shlam"></b> <b>кг/секундку</b></output>
+                                </p>
+                                 <p> Концентрація CO<sub>2</sub> на виході:
+                                  <output name="Значення"><b id="co2out"></b> <b>%</b></output>
+                                  <input name="Значення" placeholder="Specify your value (integer)" value="2%" />
+                                </p>
+                                
+                            </form>
+                                
+                                
+                                  <em>Ступінь відкриття клапана</em>
+                                <div class="progress">
+                                <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
+                                   60%
+                                </div>
+                              </div>
+                            
+                              
+                                  
+                                  <div class="btn-group" role="group">
+                                    <button type="button" class="btn btn-default dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                      Вибір графіка
+                                      <span class="caret"></span>
+                                    </button>
+                                    <ul class="dropdown-menu">
+                                      <li><a href="#">Графік зміни температури у скрубері</a></li>
+                                       <li><a href="#">Графік зміни концентрації CO<sub>2</sub> на виході</a></li>
+                                         <li><a href="#">Графік температури з Arduino</a></li>
+                                    </ul>
+                                  </div>
+                       
+                            
+                            </div>
+                        
+                    </div>
+                    
+                    <div id="chartContainer" style="height: 300px; width: 95%;"></div>
+                    
+                </div>
+
+                <footer id="footer">Гуза Катерина - КПІ &copy; 2017</footer>
+            </div>
+        </div>
+       
+        <script type="text/javascript" src="js/chart-mock.js"></script>
+    </body>
+</html>
--- js/chart-mock.js
+++ js/chart-mock.js
@@ -3,16 +3,41 @@ window.onload = function () {
     chartInit();
 }
 
+/* --- Get fresh data values --- */
+var reloadTimerId = setInterval(function() {
+    reloadData();
+}, 2000);
+
+function reloadData() {
+    /*AJAX
+    getData
+    updateDataOnPage
+     - updateCurrentValues
+     - updateChart*/
+    
+    //console.log(getRandomInt(15,25),'C');
+    $('#temp').text(getRandomInt(10,40));
+    $('#naohinp').text(getRandomInt(1, 3));
+    $('#co2inp').text(getRandomInt(10,4));
+     $('#massinp').text(getRandomInt(1,5));
+    $('#co2out').text(getRandomInt(4,1));
+    $('#shlam').text(getRandomInt(1,6));
+}
+
 /* --- Chart Mechanics --- */
 function chartInit() {
     var chart = new CanvasJS.Chart("chartContainer",
         {
+            theme: "theme1", 
             zoomEnabled: true,
             panEnabled: true,
             animationEnabled: true,
-            animationDuration: 10000,
+            animationDuration: 45000,
             title: {
-                text: "Показники вузлу №54"
+                text: "Зміна температури у скрубері в залежності від часу",
+                fontsize: 20,
+                fontColor: "#7e838d"
+       
             },
             legend: {
                 horizontalAlign: "right",
@@ -23,14 +48,19 @@ function chartInit() {
                 valueFormatString:"## t°C"
             },
             axisX: {
-                valueFormatString:"DD-MMM-YY, HH:mm"
+                //valueFormatString:"DD-MMM-YY, HH:mm",
+                valueFormatString:"### sec.",
+               
             },
             data: [{
+                
                 yValueType: "number",
-                xValueType: "dateTime",
-                type: "spline",
-                color:getColor(),
-                xValueFormatString: "DD-MMM-YY, HH:mm:ss",
+                //xValueType: "dateTime",
+                xValueType: "number",
+                type: "area",
+                color: getColor(),
+                //xValueFormatString: "DD-MMM-YY, HH:mm:ss",
+                xValueFormatString: "### sec.",
                 yValueFormatString: "## t°C",
                 dataPoints: mathModel()
             }]
@@ -43,10 +73,12 @@ function chartInit() {
 function mathModel() {
     //Initial values
     var yValue = 22;
-    var xValue = new Date(Date.parse('01 Sep 2016 11:00:00 GMT'));
+    //var xValue = new Date(Date.parse('01 Sep 2016 11:00:00 GMT'));
+    var xValue = 150;
     var dataPointsArray = [];
     dataPointsArray.push({
-        x: xValue.setMinutes(xValue.getMinutes()+30),
+        //x: xValue.setMinutes(xValue.getMinutes()+30),
+        x: xValue,
         y: yValue
     });
 
@@ -61,8 +93,10 @@ function mathModel() {
             yValue += getRandomInt(-5, 5);
         }
 
+        xValue = xValue+1;
         dataPointsArray.push({
-            x: xValue.setMinutes(xValue.getMinutes()+30),
+            //x: xValue.setMinutes(xValue.getMinutes()+30),
+            x: xValue,
             y: yValue
         });
     }
@@ -72,7 +106,7 @@ function mathModel() {
 }
 
 function getColor() {
-    var arr = ['red','orange','lime','green','black','blue','#258454','#8b29b2'];
+    var arr = ['#aabde7','#aabde7','#aabde7','#aabde7','#aabde7','#aabde7','#aabde7','#aabde7'];
     return arr[getRandomInt(0,7)];
 }
 
